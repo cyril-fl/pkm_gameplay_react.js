4a0dd101416f05fb622ec61626a03f7fe4ae867c
cyril-fl <cyril-f@hotmail.com>
2024-08-24 11:24:30 +0200

    PkdDexEntry Getter and Setter - Readonly - OK

--- src/datas/models/PkmDex.ts
+++ src/datas/models/PkmDex.ts
@@ -2,22 +2,22 @@ import { type, move } from "@customs/Interface";
 import { MOCKUP_DEX_ENTRY } from "@/datas/mockup/dex_entry";
 
 export class PkdDexEntry {
-  private _name: string;
-  private _id: number;
-  private _description: string;
-  private _types: type[];
-  private _evolution_id: string | null;
-  private _evolution_lvl: number | null;
-  private _atk_min: number;
-  private _atk_max: number;
-  private _def_min: number;
-  private _def_max: number;
-  private _hp_min: number;
-  private _hp_max: number;
-  private _spd_min: number;
-  private _spd_max: number;
-  private _moves: move[];
-  private _is_starter: boolean;
+  private readonly _name: string;
+  private readonly _id: number;
+  private readonly _description: string;
+  private readonly _types: type[];
+  private readonly _evolution_id: string | null;
+  private readonly _evolution_lvl: number | null;
+  private readonly _atk_min: number;
+  private readonly _atk_max: number;
+  private readonly _def_min: number;
+  private readonly _def_max: number;
+  private readonly _hp_min: number;
+  private readonly _hp_max: number;
+  private readonly _spd_min: number;
+  private readonly _spd_max: number;
+  private readonly _moves: move[];
+  private readonly _is_starter: boolean;
 
   constructor(entry: any = MOCKUP_DEX_ENTRY) {
     this._name = entry.name;
@@ -86,55 +86,5 @@ export class PkdDexEntry {
   }
   get isStarter(): boolean {
     return this._is_starter;
-  }
-
-  /* SET */
-  set name(data: string) {
-    this._name = data;
-  }
-  set id(data: number) {
-    this._id = data;
-  }
-  set description(data: string) {
-    this._description = data;
-  }
-  set types(data: type[]) {
-    this._types = data;
-  }
-  set evolutionId(data: string | null) {
-    this._evolution_id = data;
-  }
-  set evolutionLvl(data: number | null) {
-    this._evolution_lvl = data;
-  }
-  set atkMin(data: number) {
-    this._atk_min = data;
-  }
-  set atkMax(data: number) {
-    this._atk_max = data;
-  }
-  set defMin(data: number) {
-    this._def_min = data;
-  }
-  set defMax(data: number) {
-    this._def_max = data;
-  }
-  set hpMin(data: number) {
-    this._hp_min = data;
-  }
-  set hpMax(data: number) {
-    this._hp_max = data;
-  }
-  set spdMin(data: number) {
-    this._spd_min = data;
-  }
-  set spdMax(data: number) {
-    this._spd_max = data;
-  }
-  set moves(data: move[]) {
-    this._moves = data;
-  }
-  set isStarter(data: boolean) {
-    this._is_starter = data;
   }
 }
